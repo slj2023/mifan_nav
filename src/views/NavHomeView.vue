d43562f91915a4ae3a4636c32de80896859c85b1
slj2023 <3050325235@qq.com>
2025-08-10 13:29:23 +0800

    添加生成和左侧栏功能完善

--- src/views/NavHomeView.vue
+++ src/views/NavHomeView.vue
@@ -3,13 +3,16 @@
     <!-- 左侧边栏 -->
     <aside class="sidebar">
       <!-- Logo区域 -->
-       <button  @click="toadmin" class="logo-btn"
+       <!-- <button  @click="toadmin" class="logo-btn"
+       title="点击进入管理页面"> -->
+       <span @click="toadmin" class="logo-btn"
        title="点击进入管理页面">
       <div class="logo-section">
         <img src="/logo.png" alt="logo" class="logo" />
         <h1 class="site-title">{{ title || '米饭的导航' }}</h1>
       </div>
-      </button>
+    </span>
+      <!-- </button> -->
       <!-- 分类导航 -->
       <nav class="category-nav">
         <h2 class="nav-title">分类导航</h2>
@@ -35,8 +38,11 @@
           class="github-link"
           title="查看源代码"
         >
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+          <!-- <svg width="40" height="40" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+          </svg> -->
+          <svg width="80" height="80" viewBox="0 0 200 200" style="transform: translateY(-15px);" fill="#FFA500" xmlns="http://www.w3.org/2000/svg">
+            <path d="M40 120 Q100 180 160 120 L150 120 Q150 150 100 160 Q50 150 50 120 Z M50 120 Q40 80 70 70 Q80 50 100 55 Q120 50 130 70 Q160 80 150 120 Z M80 85 l5 5 M95 75 l5 5 M110 90 l5 5 M100 100 l5 5 M120 80 l5 5" stroke="currentColor" stroke-width="3"/>
           </svg>
           <span>一个简洁美观的个人导航网站，支持分类管理和自定义收藏夹</span>
         </a>
@@ -267,7 +273,7 @@ onUnmounted(() => {
   background: none;
   outline: none;
   width: 100%;
-  padding: 0;
+  padding: 0px;
   margin: 0;
   align-items: center;
 }
@@ -276,8 +282,13 @@ onUnmounted(() => {
   align-items: center;
   padding: 20px;
   border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+  transform: all 0.3s ease;
 }
-
+.logo-section:hover{
+  background: rgba(255, 255, 255, 0.1);
+  color: white;
+  transform: translateY(-1px);
+}
 .logo {
   width: 55px;
   height: 55px;
